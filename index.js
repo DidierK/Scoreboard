56c87375ebe75f4552048ffa17f34a481f393381
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-05-26 12:51:59 +0200

    homepage router & view

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jade = require('jade');
 
 var app = express();
+
+// view engine setup
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use('/',require('./routers/index'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
