f61479ffdc6fd7d7240c126528c51846e7bccee8
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-05-26 17:10:38 +0200

    admin management on /admin

--- routers/index.js
+++ routers/index.js
@@ -5,4 +5,18 @@ router.get('/', function(req,res) {
   res.render('index', { title: 'Live Scoreboard' });
 });
 
+router.post('/admin', function(req, res) {
+
+  var password = req.body.password;
+
+  if(password == "test"){
+    res.render('admin', { title: 'Create' });
+    console.log('Admin enter succesful');
+  }
+  else{
+    console.log('Wrong password');
+  }
+
+});
+
 module.exports = router;
