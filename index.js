3c8d4c9b2c0e974a62faaba49104005c89ee2f43
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-05-26 14:56:09 +0200

    admin route & view

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use('/',require('./routers/index'));
 app.use('/scoreboard',require('./routers/scoreboard'));
+app.use('/admin',require('./routers/admin'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
