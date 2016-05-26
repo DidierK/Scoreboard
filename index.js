edf07872e4c9e9075e618aa4af9e17c99f3d7561
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-05-26 15:39:21 +0200

    admin password control

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ var app = express();
 
 // view engine setup
 app.set('view engine', 'jade');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-app.use(bodyParser.json());
 app.use('/',require('./routers/index'));
 app.use('/scoreboard',require('./routers/scoreboard'));
 app.use('/admin',require('./routers/admin'));
--- routers/admin.js
+++ routers/admin.js
@@ -5,4 +5,18 @@ router.get('/', function(req,res) {
   res.render('admin', { title: 'Admin' });
 });
 
+router.post('/', function(req, res) {
+
+  var password = req.body.password;
+
+  if(password == "test"){
+    res.render('create', { title: 'Create' });
+    console.log('Admin enter succesful');
+  }
+  else{
+    console.log('Wrong password');
+  }
+
+});
+
 module.exports = router;
