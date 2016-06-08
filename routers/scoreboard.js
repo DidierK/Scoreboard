b97161279a4f057d907d309f51c19845ab8a9783
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-06-08 13:10:48 +0200

    scoreboard_detail view for users

--- routers/scoreboard.js
+++ routers/scoreboard.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var Game = require('../models/game');
+var Comment = require('../models/comment');
 
 router.get('/', function(req,res) {
-  res.render('scoreboard', { title: 'Live Scoreboard' });
+  var db = req.db;
+	Game.find().exec(function(err, games){
+		res.render('scoreboard', {
+			"all_games" : games,
+			"title": "Scoreboard"
+		});
+	});
+});
+
+router.get('/:id', function(req, res, next) {
+
+	// ID VAN URL
+	var game_id = req.params.id;
+
+	// ZOEK GAME OP ID
+	Game.findOne().where("_id", game_id).exec(function(err, game){
+		if (err) {
+			console.log("No discussion with given ID found.");
+			} else {
+        console.log("Game Id found");
+        Comment.find().sort({ game_comment: 'desc' }).where("stat_game_id", game_id).exec(function(err, comments){
+            console.log(comments);
+                res.render('scoreboard_detail', {
+                "all_comments" : comments,
+                game : game
+              });
+          });
+				}
+		});
+
 });
 
 module.exports = router;
