ece283322ee1b8751af8346ea54f4bb81501a617
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-06-09 17:53:25 +0200

    minify-css run

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-'use strict';
-
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('sass', function () {
-  return gulp.src('public/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/scss/scss'));
+gulp.task('minify-css', function() {
+    return gulp.src('public/stylesheets/*.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('public/stylesheets'));
 });
